52edbb2a5b5de00bf0520f68fe0ff864f75a0366
Jolaade Adewale <jolaade.adewale@andela.com>
2017-08-27 12:02:14 +0100

    single view for image have likes and profile pictures

--- src/components/dashboard/helper/singleView.js
+++ src/components/dashboard/helper/singleView.js
@@ -2,12 +2,17 @@
  * Created by jolaadeadewale on 28/07/2017.
  */
 import React from 'react';
-import {View, Image, StyleSheet, Text, TouchableHighlight} from 'react-native';
+import {View, Image, StyleSheet, Text, TouchableHighlight, Dimensions} from 'react-native';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as fileActions from '../../../actions/fileActions';
+let {height, width} = Dimensions.get('window');
+import { Ionicons } from '@expo/vector-icons';
+
+
 
 class SingleView extends React.Component {
+
 
     constructor(props) {
         super(props);
@@ -18,25 +23,30 @@ class SingleView extends React.Component {
 
     }
 
+    _onUser = (e) => {
+        console.log('I was clicked, you cut me wide iopen like land', e);
+    };
+
     render () {
         return (
 
             <View>
-                <View style={styles.hBox}>
-                    <TouchableHighlight style={styles.headerContent}>
-                        <View>
-                            <Image></Image>
-                            <Text>Username</Text>
-                        </View>
-                    </TouchableHighlight>
+                <View style={[styles.hBox, styles.contentPadding]}>
+                    <View>
+                        <TouchableHighlight style={styles.headerContent}>
+                            <View>
+                                <Circle url="" label="username" click={this._onUser} />
+                            </View>
+                        </TouchableHighlight>
+                    </View>
 
                 </View>
                 <View style={styles.imageContent}>
-                    <Image style={{width: 320, height: 260}}  source={{uri:this.state.image.secure_url}}></Image>
+                    <Image style={{width: width, height: 260}}  source={{uri:this.state.image.secure_url}}></Image>
                 </View>
                 <View  style={[styles.hBox, styles.contentPadding, styles.boxHeight]}>
                     <TouchableHighlight onPress={()=> {console.log('I was clicked')}}>
-                        <Image style={{width: 20, height: 20}} source={require('../../../images/likee.png')}></Image>
+                        <Ionicons name="ios-heart-outline" size={20}  />
                     </TouchableHighlight>
                     <TouchableHighlight onPress={()=> {console.log('I was clicked too')}} style={styles.imageRight}>
                         <Image style={{width: 20, height: 20}} source={require('../../../images/chat.png')}></Image>
@@ -46,9 +56,6 @@ class SingleView extends React.Component {
 
                 </View>
                 <View style={[styles.hBox, styles.contentPadding]}>
-                    <TouchableHighlight style={styles.imageText}>
-                        <Image style={{width: 8, height: 8}} source={require('../../../images/likee.png')}></Image>
-                    </TouchableHighlight>
                     <TouchableHighlight>
                         <Text style={styles.textSize}>2000 Likes</Text>
                     </TouchableHighlight>
@@ -61,15 +68,32 @@ class SingleView extends React.Component {
                         <Text>Commments</Text>
                     </TouchableHighlight>
                 </View>
-                <View style={styles.contentPadding}>
-                    <Text style={styles.commentText}>View all comments </Text>
-                    <Text style={styles.commentText}>2 hours ago</Text>
-                </View>
             </View>
         )
     }
-
 }
+
+const Circle = ({label, url, click}) => {
+    return (
+        <View style={styles.center}>
+            <TouchableHighlight onPress={() => click(label)}>
+                <View style={{flexDirection: 'row', flex: 1, justifyContent: 'center'}}>
+                    <View style={styles.circle}>
+                        <Image  style={{width: 36, height: 36, borderRadius: 36/2,}}
+                            source={{ uri:
+                            'http://res.cloudinary.com/dd58mfinr/image/upload/v1481734664/default.png' || url} } />
+                    </View>
+                    <View>
+                        <Text onPress={()=> click(label)} style={styles.text}>{label}</Text>
+                        <Text style={{justifyContent: 'flex-end', fontSize: 8}}>Follow</Text>
+                    </View>
+                </View>
+
+            </TouchableHighlight>
+        </View>
+    )
+};
+
 
 const styles = StyleSheet.create({
     hBox: {
@@ -91,7 +115,7 @@ const styles = StyleSheet.create({
         marginBottom: 20
     },
     imageContent: {
-        marginTop: 30
+        marginTop: 70
     },
 
     imageRight: {
@@ -116,6 +140,23 @@ const styles = StyleSheet.create({
         color: 'grey',
         marginTop: 10,
         fontSize: 10
+    },
+    circle: {
+        width: 36,
+        height: 36,
+        borderRadius: 36/2,
+        backgroundColor: '#D3D3D3',
+        marginRight: 6
+    },
+    center: {
+        marginTop: 10,
+        alignContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        marginTop: 9,
+        fontSize: 8,
+        textAlign: 'center'
     }
 
 });
