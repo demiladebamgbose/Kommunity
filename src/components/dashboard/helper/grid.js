c4a1c07eccd69b5e799c435bc406c388723361dd
Jolaade Adewale <jolaade.adewale@andela.com>
2017-08-28 23:41:20 +0100

    pull full images

--- src/components/dashboard/helper/grid.js
+++ src/components/dashboard/helper/grid.js
@@ -5,23 +5,23 @@ import React from 'react';
 import {View, Image, TouchableHighlight} from 'react-native';
 
 const Grid = ({obj, click, width}) => {
-   /* let data1 = obj['0'].content.secure_url.split('upload/');
+    let data1 = obj['0'].content.secure_url.split('upload/');
     data1  = data1.join(`upload/c_scale,h_100,w_${(width - 1)}/`);
     let data2, data3 = '';
 
     if(!obj['1']) {
         obj['1'] = {'content': {'secure_url': ''}}
     } else{
-        data2 = obj['1'].content.secure_url.split('upload/');
-        data2 = data2.join(`upload/c_scale,h_100,w_${(width - 1)}/`);
+     //   data2 = obj['1'].content.secure_url.split('upload/');
+      //  data2 = data2.join(`upload/c_scale,h_100,w_${(width - 1)}/`);
     }
 
     if(!obj['2']) {
         obj['2'] = {'content': {'secure_url': ''}};
     } else{
-        data3 = obj['2'].content.secure_url.split('upload/');
-        data3 = data3.join(`upload/c_scale,h_100,w_${(width - 1)}/`);
-    } */
+      //  data3 = obj['2'].content.secure_url.split('upload/');
+        // data3 = data3.join(`upload/c_scale,h_100,w_${(width - 1)}/`);
+    }
 
     return (
         <View key={obj['0'].content} style={{flex: 1, flexDirection: 'row'}}>
@@ -37,7 +37,7 @@ const Grid = ({obj, click, width}) => {
             </TouchableHighlight>
             <TouchableHighlight onPress={() => click(obj['2']._id)}>
                 <View style={{ width: width, height: 100,   margin: 1}} >
-                    <Image style={{width: (width - 1), height: 99}}  source={{ uri: obj['2'].content.secure_url} } />
+                    <Image style={{width: (width - 1), height: 99}}  source={{ uri: obj['2'].content.secure_url } } />
                 </View>
             </TouchableHighlight>
         </View>
