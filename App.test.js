bcc518d591fc076a549d576f28ef04a335052b68
Jolaade Adewale <jolaade.adewale@andela.com>
2018-06-05 01:14:27 +0100

    Add Mock up test

--- App.test.js
+++ App.test.js
@@ -1,8 +1,17 @@
+jest.mock('./src/api/userApi');
 import React from 'react';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
 
-it('renders correctly without crashing', () => {
-  const wrapper = mount(<App/>);
+describe('Adds Root component', () => {
+  const wrapper = shallow((
+    <App
+    />
+  ));
+
+  it('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
 
 });
